e30f95873cf711d519af5021c83a35d968920e24
NazarkoKril <122899742+NazarkoKril@users.noreply.github.com>
2024-11-27 19:09:46 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,7 @@
 // swiper 
-const swiper = new Swiper(".main_hero_swiper", {
-    loop: true,
-    pagination: {
-        el: ".pagination_main_hero",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".next_main_hero",
-        prevEl: ".prev_main_hero",
-    },
-    slidesPerView: 1,
-    speed: 1000,
-
+var splide = new Splide('.splide', {
+    type: 'loop',
+    arrowPath: 'M6.34576 31L0 31L-6.77527e-07 15.5L-1.35505e-06 0L6.34576 -7.2751e-07L19.0373 15.5L6.34576 31Z',
 });
+
+splide.mount();
