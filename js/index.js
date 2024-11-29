c36c3475f10cbd663b53b1c83ac54c7fb64dbe8f
NazarkoKril <122899742+NazarkoKril@users.noreply.github.com>
2024-11-29 18:56:58 +0200

    update

--- js/index.js
+++ js/index.js
@@ -40,10 +40,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-
+// like click
 document.querySelectorAll('.like, .shop').forEach(button => {
     button.addEventListener('click', () => {
         button.classList.toggle('active');
     });
 });
 
+// video 
+document.addEventListener("DOMContentLoaded", () => {
+    const video = document.getElementById("video_main");
+    const observer = new IntersectionObserver(([entry], obs) => {
+        if (entry.isIntersecting) {
+            const source = document.createElement("source");
+            source.src = "./video/video_main.mp4";
+            source.type = "video/mp4";
+            video.appendChild(source);
+            obs.disconnect();
+        }
+    });
+    observer.observe(video);
+});
